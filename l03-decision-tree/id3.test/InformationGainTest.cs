c55b4ed73521753c7e8c136f37873b14febb6ebb
Mahmoud Rawas <mhmoudr@gmail.com>
2019-01-04 15:50:25 +1100

    Add more test to information gain.

--- l03-decision-tree/id3.test/InformationGainTest.cs
+++ l03-decision-tree/id3.test/InformationGainTest.cs
@@ -14,10 +14,22 @@ namespace Tests
         }
 
         [Test]
-        public void InformationGainReturnsCorrect()
+        public void InformationGainReturnsCorrectForWindFactor()
         {
             var e = InformationGain.Calculate(data, "IsLate", "Wind");
             Assert.That(e,Is.EqualTo(0.048).Within(0.001));
+        }
+        [Test]
+        public void InformationGainReturnsCorrectForOutlookFactor()
+        {
+            var e = InformationGain.Calculate(data, "IsLate", "Outlook");
+            Assert.That(e,Is.EqualTo(0.246).Within(0.001));
+        }
+        [Test]
+        public void InformationGainReturnsCorrectForHumidityFactor()
+        {
+            var e = InformationGain.Calculate(data, "IsLate", "Humidity");
+            Assert.That(e,Is.EqualTo(0.151).Within(0.001));
         }
     }
 }
