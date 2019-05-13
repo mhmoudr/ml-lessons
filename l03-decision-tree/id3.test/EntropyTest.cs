5140f36d8694d853c03c3bc3b722446e713552c8
Mahmoud Rawas <mhmoudr@gmail.com>
2019-05-13 20:58:38 +1000

    Improve Entropy implementation without breaking interface.

--- l03-decision-tree/id3.test/EntropyTest.cs
+++ l03-decision-tree/id3.test/EntropyTest.cs
@@ -19,5 +19,23 @@ namespace Tests
             var e = Entropy.Calculate(data, "IsLate");
             Assert.That(e, Is.EqualTo(0.940).Within(0.001));
         }
+        [Test]
+        public void TestMinEntropy()
+        {
+            var data = new[] { "A", "A", "A", "A", "A", "A", "A" };
+            Assert.AreEqual(0d, Entropy.Calculate(data), 0.0001);
+        }
+        [Test]
+        public void TestHighEntropy()
+        {
+            var data = new[] { "A", "A", "B", "B", "C", "C", "D", "D" };
+            Assert.AreEqual(2d, Entropy.Calculate(data), 0.0001);
+        }
+        [Test]
+        public void TestHighEntropy2()
+        {
+            var data = new[] { "A", "A", "A", "A", "C", "C", "D", "F" };
+            Assert.AreEqual(1.75d, Entropy.Calculate(data), 0.0001);
+        }
     }
 }
--- l03-decision-tree/id3/Entropy.cs
+++ l03-decision-tree/id3/Entropy.cs
@@ -9,12 +9,19 @@ namespace id3
         public static double Calculate(Data data, string targetColumn)
         {
             var idx = data.Columns[targetColumn];
-            var count = data.Rows.Count;
-            return data.Rows.GroupBy(r => r[idx]).Select(g =>
-            {
-                var p = (double) g.Count() / count;
-                return -p * Math.Log(p, 2);
-            }).Sum();
+            return Calculate(data.Rows.Select(r=>r[idx]).ToArray());
         }
+        public static double Calculate(string[] column)
+        {
+            return column
+                .GroupBy(v => v)
+                .Select(g =>
+                {
+                    var p = (double) g.Count() / column.Length;
+                    return -p * Math.Log(p, 2);
+                })
+                .Sum();
+        }
+
     }
 }
