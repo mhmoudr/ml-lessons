bfe8175f89aab5dea1bd96b8a34ce26e86a3da7f
Mahmoud Rawas <mhmoudr@gmail.com>
2019-05-13 21:16:25 +1000

    Simplify information gain implementation withour breaking API, and add more test cases.

--- l03-decision-tree/id3.test/InformationGainTest.cs
+++ l03-decision-tree/id3.test/InformationGainTest.cs
@@ -17,19 +17,31 @@ namespace Tests
         public void InformationGainReturnsCorrectForWindFactor()
         {
             var e = InformationGain.Calculate(data, "IsLate", "Wind");
-            Assert.That(e,Is.EqualTo(0.048).Within(0.001));
+            Assert.That(e, Is.EqualTo(0.048).Within(0.001));
         }
         [Test]
         public void InformationGainReturnsCorrectForOutlookFactor()
         {
             var e = InformationGain.Calculate(data, "IsLate", "Outlook");
-            Assert.That(e,Is.EqualTo(0.246).Within(0.001));
+            Assert.That(e, Is.EqualTo(0.246).Within(0.001));
         }
         [Test]
         public void InformationGainReturnsCorrectForHumidityFactor()
         {
             var e = InformationGain.Calculate(data, "IsLate", "Humidity");
-            Assert.That(e,Is.EqualTo(0.151).Within(0.001));
+            Assert.That(e, Is.EqualTo(0.151).Within(0.001));
+        }
+        [Test]
+        public void InformationGainForTrivial1()
+        {
+            var data = new (string, string)[] { ("Y", "A"), ("Y", "A"), ("Y", "A"), ("Y", "A") };
+            Assert.AreEqual(0d, InformationGain.Calculate(data), 0.0001);
+        }
+        [Test]
+        public void InformationGainForHighParity()
+        {
+            var data = new (string, string)[] { ("Y", "A"), ("Y", "A"), ("N", "B"), ("N", "B") };
+            Assert.AreEqual(1d, InformationGain.Calculate(data), 0.0001);
         }
     }
 }
--- l03-decision-tree/id3/InformationGain.cs
+++ l03-decision-tree/id3/InformationGain.cs
@@ -5,27 +5,18 @@ namespace id3
 {
     public static class InformationGain
     {
-        public static double Calculate(Data data, string labelColumn,string targetColumn)
+        public static double Calculate(Data data, string labelColumn, string targetColumn)
         {
             var tgtIdx = data.Columns[targetColumn];
             var lblIdx = data.Columns[labelColumn];
-            var labelEntropy = Entropy.Calculate(data, labelColumn);
-            var dataCount = (double)data.Rows.Count;
-            var sumResult = data.Rows.GroupBy(r => r[tgtIdx]).Select(tgtGroup =>
-            {
-                var tgtGroupCount = (double)tgtGroup.Count();
-                Console.WriteLine($"grouping by {targetColumn} / {tgtGroup.Key}: count was: {tgtGroupCount}");
-                var tgtGroupEntropy = tgtGroup.GroupBy(gg=>gg[lblIdx]).Select(g =>
-                {
-                    var p = g.Count() / tgtGroupCount;
-                    Console.WriteLine($"probability of {g.Key} is {p}");
-                    return -p * Math.Log(p, 2);
-                }).Sum();
-                Console.WriteLine($"target group entropy={tgtGroupEntropy}");
-                return (tgtGroupCount / dataCount) * tgtGroupEntropy;
-            }).Sum();
-            Console.WriteLine($"entropy: {labelEntropy} and sub Entropy:{sumResult}");
-            return labelEntropy - sumResult;
+            return Calculate(data.Rows.Select(r => (r[tgtIdx], r[lblIdx])).ToArray());
+        }
+        public static double Calculate((string y, string x)[] data)
+        {
+            return Entropy.Calculate(data.Select(r => r.y).ToArray()) -
+                data.GroupBy(r => r.x)
+                    .Select(g => ((double)g.Count() / data.Length) * Entropy.Calculate(g.Select(r => r.y).ToArray()))
+                    .Sum();
         }
     }
 }
