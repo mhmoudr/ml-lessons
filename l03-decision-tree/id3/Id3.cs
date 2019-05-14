fd9687af34695407189f87adbd4df4c873a13e5e
Mahmoud Rawas <mhmoudr@gmail.com>
2019-05-14 13:55:10 +1000

    new simplified implementation.(same logic)

--- l03-decision-tree/id3/Id3.cs
+++ l03-decision-tree/id3/Id3.cs
@@ -2,60 +2,47 @@ using System.Collections.Generic;
 using System.Linq;
 namespace id3
 {
-    public static class Id3
+    public class Id3
     {
-        public static Node Train(Data data, string labelColumn, string[] features, int maxDepth = 10)
+        public static Tree Train(Data data, int MaxDepth)
         {
-            var node = new Node(data);
-            Train(node, labelColumn, features, 0, maxDepth);
-            return node;
+            var tree = new Tree();
+            Train(tree, data, 1, MaxDepth);
+            return tree;
         }
-        private static void Train(Node node, string labelColumn, string[] features, int currentLevel, int maxDepth)
+        private static void Train(SplitNode node, Data data, int currentLevel, int MaxDepth)
         {
-            var colWithMaxGain = FindBestSplit(node, labelColumn, features);
-            node.SplittingColumn = colWithMaxGain;
-            var possibleLeaf = IsItLeaf(node, labelColumn, currentLevel, maxDepth);
-            if (possibleLeaf != null)
-                node.Prediction = possibleLeaf;
-            else
-            {
-                node.Children = GenerateChildrenNodes(node);
-                foreach (var n in node.Children)
-                    Train(n.Value, labelColumn, features, currentLevel + 1, maxDepth);
-            }
-        }
-        private static string IsItLeaf(Node node, string labelColumn, int currentLevel, int maxDepth)
-        {
-            var lblIdx = node.Data.Columns[labelColumn];
-            var labels = node.Data.Rows.Select(r => r[lblIdx]);
-            string leafValue = null;
-            if (currentLevel >= maxDepth)
-            {
-                leafValue = labels.GroupBy(l => l).OrderByDescending(g => g.Count()).First().Key;
-            }
-            else
-            {
-                var first = labels.First();
-                leafValue = (labels.All(l => l == first)) ? first : null;
-            }
-            return leafValue;
-        }
-        private static (string col, double gain) FindBestSplit(Node node, string labelColumn, string[] features)
-        {
-            var gains = features.Select(col => (col, gain: InformationGain.Calculate(node.Data, labelColumn, col)));
-            return gains.Aggregate((l, r) => l.gain > r.gain ? l : r); // similar to fold function in FP
-        }
-        private static Dictionary<string, Node> GenerateChildrenNodes(Node node)
-        {
-            var colIdx = node.Data.Columns[node.SplittingColumn.col];
-            return node.Data.Rows
-                .GroupBy(r => r[colIdx])
+            var bestSplit = BestSplitColumn(data);
+            var splitColIxd = data.Columns[bestSplit.columnName];
+            node.Gain = bestSplit.gain;
+            node.SplittingColumn = bestSplit.columnName;
+            node.Children = data.Rows
+                .GroupBy(r => r.factors[splitColIxd])
                 .Select(g =>
-                (
-                    colName: g.Key,
-                    Node: new Node(new Data(node.Data.Columns, g.ToList()))
-                ))
-                .ToDictionary(i => i.colName, i => i.Node);
+                {
+                    // provide predition with the most frequent response 
+                    if (currentLevel >= MaxDepth)
+                    {
+                        Node res = new LeafNode() { Response = g.GroupBy(r => r.lable).OrderByDescending(gg => gg.Count()).First().Key };
+                        return (g.Key, res);
+                    }
+                    //check if all responses are the same
+                    var l0 = g.First().lable;
+                    if (g.All(r => r.lable == l0))
+                        return (g.Key, new LeafNode() { Response = l0 });
+                    var newNode = new SplitNode();
+                    Train(newNode, new Data() { 
+                            Columns = data.Columns ,//.Where(c=>c.Key != node.SplittingColumn).ToDictionary(i=>i.Key,i=>i.Value), 
+                            Rows = g.ToArray() }, 
+                        currentLevel + 1, MaxDepth);
+                    return (g.Key, newNode);
+                })
+                .ToDictionary(i => i.Key, i => i.res);
+
+        }
+        private static (string columnName, double gain) BestSplitColumn(Data data)
+        {
+            return data.Columns.Select(col => (col.Key, InformationGain.Calculate(data, col.Key))).OrderByDescending(i => i.Item2).First();
         }
     }
 }
--- l03-decision-tree/id3/Program.cs
+++ l03-decision-tree/id3/Program.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using System.Linq;
 namespace id3
 {
     class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            var data = Repository.GetTrainStatusData();
-            var label = "IsLate";
-            var features = data.Columns.Keys.Where(c => c != label && c != "Day").ToArray();
-            var node = Id3.Train(data, label, features);
-            node.print();
+            var featuresNum = 3;
+            var lines = File.ReadLines("../train.csv");
+            var data = new Data()
+            {
+                Columns = lines.First().Split(",").Select((value, index) => (value, index)).Take(featuresNum).ToDictionary(i => i.value, i => i.index),
+                Rows = lines.Skip(1).Select(l => l.Split(",")).Select(r => (r[featuresNum], r.Take(featuresNum).ToArray())).ToArray()
+            };
+            var model = Id3.Train(data, 10);
+            var result = model.Predict(data.Columns, new[] { "AfterPublicHoliday", "Rain", "Strong" });
+            Console.WriteLine($"You prediction is: {result}");
+            model.Print();
         }
     }
 }
