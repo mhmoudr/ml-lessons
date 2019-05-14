aa0fa1f06312f29a6ec21b2f3de6149afeea6779
Mahmoud Rawas <mhmoudr@gmail.com>
2019-05-14 13:54:40 +1000

    Simplify data structure

--- l03-decision-tree/id3/Data.cs
+++ l03-decision-tree/id3/Data.cs
@@ -4,18 +4,7 @@ namespace id3
 {
     public class Data
     {
-        public readonly Dictionary<string, int> Columns;
-        public readonly List<string[]> Rows;
-
-        public Data()
-        {
-            Columns = new Dictionary<string, int>();
-            Rows = new List<string[]>();
-        }
-        public Data(Dictionary<string, int> columns, List<string[]> rows)
-        {
-            Columns = columns;
-            Rows = rows;
-        }
+        public Dictionary<string, int> Columns;
+        public (string lable , string[] factors)[] Rows;
     }
 }
